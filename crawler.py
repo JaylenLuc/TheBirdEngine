083b0f699e8f326169dbdddbbec3455057955091
jhluc <jhluc@uci.edu>
2023-07-03 15:38:52 -0700

    tokenizer done, indexer done and everything in between

--- crawler.py
+++ crawler.py
@@ -11,11 +11,14 @@ import frontier
 import urllib
 import tokenizer
 import re
-
+from krovetzstemmer import Stemmer
+#stopwords we do at query time
+#from nltk.corpus import stopwords
+#stop_words = set(stopwords.words('english'))
 
 class CrawlerThread:
 
-
+    inverted_matrix = dict() #dict{term : dict{URLS : list[positions]}}
 
     def __init__(self) -> None:
         self.frontier = frontier.Frontier()
@@ -24,17 +27,41 @@ class CrawlerThread:
         
         while self.frontier.queue:
             current_url = self.frontier.dequeue()
-            #look at robot file
             
+            #look at robot file
+
             #construct bs4 object
             bs_obj = bs4.BeautifulSoup(current_url, 'html.parser')
             #get the text
-            tokenized = tokenizer.Tokenizer.tokenize(bs_obj)
+            if tokenizer.Tokenizer.check_relevance(bs_obj):
+                #parsing
+                tokenized = tokenizer.Tokenizer.tokenize(bs_obj.get_text())
+            else: 
+                tokenized = []
+
             if not tokenized:
                 #get all links
                 self.frontier.queue.extend(bs_obj.find_all('a'))
-    
 
+                #we deal with stopwords at query time but stemming at indexing
+                # we stem all indices and we stem the query adn apply stopwords to the query
+                for index, token in enumerate(tokenized):
+                    new_token = tokenizer.Tokenizer.token_conflation(token)
+                    #start stemming 
+                    stemmed_word = tokenizer.Tokenizer.stem_word(new_token)
+
+                    #add to inverted term matrix
+                    try:
+                        CrawlerThread.inverted_matrix[stemmed_word][current_url].append(index)
+
+                    except KeyError:
+                        CrawlerThread.inverted_matrix[stemmed_word] = dict()
+                        CrawlerThread.inverted_matrix[stemmed_word][current_url] = []
+                        CrawlerThread.inverted_matrix[stemmed_word][current_url].append(index)
+
+
+                
+        
 
 
 
--- tokenizer.py
+++ tokenizer.py
@@ -3,30 +3,39 @@
 #get all the pictures -- but thats for later
 import bs4
 import re
-from nltk.corpus import stopwords
+from krovetzstemmer import Stemmer
 class Tokenizer:
-
-    stop_words = set(stopwords.words('english'))
 
     @staticmethod
     def check_relevance(text : bs4.BeautifulSoup) -> bool:
         #check if bird is in title 
         if re.search(r'(bird|birds|avian|avians)+', text.title.string.lower() ):
             return True
-
+        #if not in title then bird is checked for at least once in the text
         return re.search(r'(bird|birds|avian|avians)+',text.get_text().lower())
         
         
-    
+    #could be used to tokenize both query and html 
     @staticmethod
-    def tokenize(text : bs4.BeautifulSoup) -> list:
-        if Tokenizer.check_relevance(text):
-            #parsing
-            unparsed_tokens = text.get_text().lower().split()
-            #remove stops 
-            #stem all the words and do not count stop words 
+    def tokenize(text : str) -> list:
+       
+        #parsing
 
+        unparsed_tokens = text.lower().split()
 
+        return list(filter(None,re.split(r"\s|;|:|\"|\“|\”|,|-|—|\!|\?|\.\s+",unparsed_tokens)))
+    
+        #parsing leaves apotrophies and periods within words alone
 
-            pass
-        else: return []
+    #probobly should be in for loop
+    @staticmethod
+    def token_conflation(token : str) -> str:
+        #conflates all characters after the apotrophie and guts all periods 
+        return re.sub("'", '', re.sub('\.', '', token))
+
+    @staticmethod
+    def stem_word(token : str) -> str:
+        stemmer = Stemmer()
+        return stemmer.stem(token)
+         
+       
